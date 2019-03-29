12f09f8cb023f8d2f6907832ed189bbf03ef0e6d
Nikolay Sarychev <nikolaysarychev@gmail.com>
2019-03-29 13:58:15 -0600

    Add func and struct

--- intro/main.go
+++ intro/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		os.Exit(1)
-	}
-	fmt.Println("It's over", os.Args[1])
+	power := getPower()
+	goku := Saiyan{Name: "Goku"}
+	goku.Power = power
+	log(goku)
+}
+
+func getPower() int {
+	return add(9001, 1)
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func log(goku Saiyan) {
+	fmt.Printf("Name: %s, Power %d\n", goku.Name, goku.Power)
+}
+
+type Saiyan struct {
+	Name  string
+	Power int
 }
