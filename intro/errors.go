920b38b0ec020bd76efd7013740283db8b32e12f
Nikolay Sarychev <nikolaysarychev@gmail.com>
2019-04-04 09:54:15 -0600

    Add errors

---
+++ intro/errors.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func errorsRun() {
+	arr := []string{"blah", "7"}
+	if len(arr) != 2 {
+		os.Exit(1) // dead code, just an example
+	}
+
+	n, err := strconv.Atoi(arr[1])
+	if err != nil {
+		fmt.Println("not a valid number")
+	} else {
+		fmt.Println(n)
+	}
+
+	analyze(1)
+	analyze(0)
+}
+
+func analyze(i int) {
+	error := isGteOne(i)
+	if error == nil {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println(error.Error())
+	}
+}
+
+type error interface {
+	Error() string
+}
+
+func isGteOne(count int) error {
+	if count < 1 {
+		return errors.New("No")
+	}
+	return nil
+}
