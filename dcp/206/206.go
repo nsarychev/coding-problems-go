01ed18ed14c32d48af206eb626f623016035af3a
Nikolay Sarychev <nikolaysarychev@gmail.com>
2019-04-05 15:28:39 -0600

    Implement 206

--- dcp/206/206.go
+++ dcp/206/206.go
@@ -9,8 +9,13 @@ type item struct {
 }
 
 func solution(input []*item, perm []int) ([]*item, error) {
+	result := make([]*item, len(input))
 	if len(input) != len(perm) {
 		return nil, fmt.Errorf("input size (%d) != perm size (%d)", len(input), len(perm))
 	}
-	return input, nil
+	for index, p := range perm {
+		result[index] = input[p]
+		result[p] = input[index]
+	}
+	return result, nil
 }
--- dcp/206/206_test.go
+++ dcp/206/206_test.go
@@ -16,15 +16,27 @@ func TestSolution(t *testing.T) {
 			Value: "c",
 		},
 	}
+	expected := []*item{
+		&item{
+			Value: "c",
+		},
+		&item{
+			Value: "b",
+		},
+		&item{
+			Value: "a",
+		},
+	}
 	perm := []int{2, 1, 0}
 
 	result, err := solution(input, perm)
 	if err != nil {
 		t.Errorf("206, for input [%v], perm [%v] -> error  %v", input, perm, err)
 	}
-	for index, value := range result {
-		if value.Value != input[index].Value {
-			t.Errorf("206, for index [%d] = %v, want %v", index, value.Value, input[index].Value)
+	for index, r := range result {
+		e := expected[index]
+		if r.Value != expected[index].Value {
+			t.Errorf("206, for index [%d] = %v, want %v", index, r.Value, e.Value)
 		}
 	}
 
